bdc51b4eb0ad0eba01636af55f1d7feed05b6b23
DevTimlas <timilehinoladejo@yahoo.com>
2022-11-07 12:53:30 +0100

    just json

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
 import json
+import os
 
-app = FastAPI(debug=True)
+app = FastAPI()
 
 jso = "database.json"
 
@@ -9,3 +10,6 @@ jso = "database.json"
 async def root():
 	json_data = json.loads(open(jso).read())
 	return json_data
+	
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
