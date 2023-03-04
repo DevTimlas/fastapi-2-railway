ff47c78efd737b140a7e5705358fd9c850f5ea09
Timilehin <timilehinoladejo@yahoo.com>
2023-03-04 08:42:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from hypercorn.config import Config
 from hypercorn.asyncio import serve
 
 app = FastAPI()
-"""
 
 jso = "./database.json"
 
@@ -20,7 +19,10 @@ async def root():
 	return json_data
 	
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+	config = Config()
+	config.bind = ["0.0.0.0:5000"]
+	asyncio.run(serve(app, config))
+    # uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
 """
 
 @app.get("/")
@@ -63,3 +65,4 @@ if __name__ == '__main__':
 	config = Config()
 	config.bind = ["0.0.0.0:5000"]
 	asyncio.run(serve(app, config))
+"""
