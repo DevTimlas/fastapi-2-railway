8c4bf97a844691f93d41a38994ab2ab84ee6b8af
Timilehin <timilehinoladejo@yahoo.com>
2023-03-04 08:59:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-from fastapi import FastAPI, File, UploadFile
+import os
+from typing import Optional
+from fastapi import FastAPI
+import uvicorn
+import random
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"Hello": "World from FastAPI"}
+
+# get random number between min(default:0) and max(default:9)
+@app.get("/random/")
+def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
+    rval = random.randint(min, max)
+    return { "value": rval }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+
+
+"""from fastapi import FastAPI, File, UploadFile
 import json
 import os
 import asyncio
@@ -23,7 +45,7 @@ if __name__ == "__main__":
 	# config = Config()
 	# config.bind = ["0.0.0.0:5000"]
 	# asyncio.run(serve(app, config))
-"""
+
 
 @app.get("/")
 async def root():
